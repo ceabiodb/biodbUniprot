c9503da11f141c59d820b55849f1e2eed5052fb2
Pierrick Roger <pk.roger@icloud.com>
2021-09-10 10:04:36 +0200

    Generate new entry ref files (#14)
    * Upgrade maintenance files.
    
    * Generate new entry ref files for testing of field parsing.
    
    * Split conversion tests. Check null entry in conversion.
    
    * Increase biodb version.

--- longtests/testthat/test_long_200_conversions.R
+++ longtests/testthat/test_long_200_conversions.R
@@ -1,9 +1,12 @@
-test.geneSymbolsToUniprotIds <- function(conn) {
+test_geneSymbolsToUniprotIds_null <- function(conn) {
     
     # Null list
     testthat::expect_equal(conn$geneSymbolToUniprotIds(NULL), list())
     testthat::expect_equal(conn$geneSymbolToUniprotIds(character()), list())
     testthat::expect_equal(conn$geneSymbolToUniprotIds(NA_character_), list())
+}
+
+test_geneSymbolsToUniprotIds_exact <- function(conn) {
 
     # Exact search
     expected_ids <- list('TGF-b1'='Q4LDM3')
@@ -14,6 +17,9 @@ test.geneSymbolsToUniprotIds <- function(conn) {
         testthat::expect_true(all(expected_ids[[gene]] %in% ids[[gene]]))
         testthat::expect_false(any(not_expected_ids[[gene]] %in% ids[[gene]]))
     }
+}
+
+test_geneSymbolsToUniprotIds_exact_2_genes <- function(conn) {
 
     # Exact search with two genes
     expected_ids <- list('TGF-b1'='Q4LDM3', 'G-CSF'='Q9GJU0')
@@ -26,6 +32,9 @@ test.geneSymbolsToUniprotIds <- function(conn) {
         testthat::expect_true(all(expected_ids[[gene]] %in% ids[[gene]]))
         testthat::expect_false(any(not_expected_ids[[gene]] %in% ids[[gene]]))
     }
+}
+
+test_geneSymbolsToUniprotIds_ignore_alphanum <- function(conn) {
 
     # Ignore non-alphanum chars (e.g.: "G-CSF" == "GCSF")
     expected_ids <- list('TGF-b1'='Q4LDM3', 'G-CSF'=c('Q9GJU0', 'P09919'))
@@ -38,7 +47,10 @@ test.geneSymbolsToUniprotIds <- function(conn) {
         testthat::expect_true(all(expected_ids[[gene]] %in% ids[[gene]]))
         testthat::expect_false(any(not_expected_ids[[gene]] %in% ids[[gene]]))
     }
+}
     
+test_geneSymbolsToUniprotIds_partial_match <- function(conn) {
+
     # Partial match (e.g.: "G-CSFb1" matches when searching for "G-CSF")
     expected_ids <- list('TGF-b1'=c('Q4LDM3', 'W1I9X7'),
                          'G-CSF'=c('Q9GJU0', 'A0A3G2Y4F6', 'C0STS3'))
@@ -50,6 +62,9 @@ test.geneSymbolsToUniprotIds <- function(conn) {
         testthat::expect_true(all(expected_ids[[gene]] %in% ids[[gene]]))
         testthat::expect_false(any(not_expected_ids[[gene]] %in% ids[[gene]]))
     }
+}
+
+test_geneSymbolsToUniprotIds_no_filtering <- function(conn) {
 
     # No filtering
     expected_ids <- list('TGF-b1'=c('Q4LDM3', 'W1I9X7'),
@@ -75,7 +90,17 @@ conn <- biodb$getFactory()$createConn('uniprot')
 
 # Run tests
 biodb::testThat('We can convert gene symbols to UniProt IDs.',
-                test.geneSymbolsToUniprotIds, conn=conn)
+                test_geneSymbolsToUniprotIds_null, conn=conn)
+biodb::testThat('We can convert gene symbols to UniProt IDs.',
+                test_geneSymbolsToUniprotIds_exact, conn=conn)
+biodb::testThat('We can convert gene symbols to UniProt IDs.',
+                test_geneSymbolsToUniprotIds_exact_2_genes, conn=conn)
+biodb::testThat('We can convert gene symbols to UniProt IDs.',
+                test_geneSymbolsToUniprotIds_ignore_alphanum, conn=conn)
+biodb::testThat('We can convert gene symbols to UniProt IDs.',
+                test_geneSymbolsToUniprotIds_partial_match, conn=conn)
+biodb::testThat('We can convert gene symbols to UniProt IDs.',
+                test_geneSymbolsToUniprotIds_no_filtering, conn=conn)
 
 # Terminate Biodb
 biodb$terminate()
